ac59f4d071ec0a6a64d31a1eb5fadd55d3057e47
JacobChurch2 <jchurch@student.neumont.edu>
2024-10-29 17:29:58 -0600

    Phase2 Destroys platforms

--- Assets/Scripts/Boss Stuff/LustBossControllerPhaseTwo.cs
+++ Assets/Scripts/Boss Stuff/LustBossControllerPhaseTwo.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Splines;
+using UnityEngine.Tilemaps;
 
 [RequireComponent(typeof(NavMeshAgent))]
 [RequireComponent(typeof(Rigidbody2D))]
@@ -21,6 +22,15 @@ public class LustBossControllerPhaseTwo : MonoBehaviour
 
 	[SerializeField]
 	BoxCollider2D Bounds;
+
+	[SerializeField]
+	GridLayout GridLayout;
+
+	[SerializeField]
+	Tilemap Tilemap;
+
+	[SerializeField]
+	ParticleSystem ParticleSystem;
 
 	NavMeshAgent agent;
 	Rigidbody2D rb;
@@ -65,7 +75,12 @@ public class LustBossControllerPhaseTwo : MonoBehaviour
 			StartCoroutine(Dash());
 			agent.SetDestination(Target.position);
 		}
+		else
+		{
+			DestoryTiles();
+		}
 		ClampPositionToScreen();
+
 	}
 
 	private IEnumerator Dash()
@@ -91,6 +106,23 @@ public class LustBossControllerPhaseTwo : MonoBehaviour
 		float minY = Bounds.transform.position.y - Bounds.size.y * 0.5f;
 		float maxX = Bounds.transform.position.x + Bounds.size.x * 0.5f;
 		float maxY = Bounds.transform.position.y + Bounds.size.y * 0.5f;
-		transform.position = new Vector3 (Math.Clamp(transform.position.x, minX, maxX), Math.Clamp(transform.position.y, minY, maxY), transform.position.z);
+		transform.position = new Vector3(Math.Clamp(transform.position.x, minX, maxX), Math.Clamp(transform.position.y, minY, maxY), transform.position.z);
+	}
+
+	private void DestoryTiles()
+	{
+		for (int i = -1; i <= 1; i++)
+		{
+			for (int j = -1; j <= 1; j++)
+			{
+				Vector3Int position = GridLayout.WorldToCell(transform.position);
+				TileBase tile = Tilemap.GetTile(new Vector3Int (position.x + i, position.y + j, position.z));
+				if (tile)
+				{
+					Instantiate(ParticleSystem, transform.position, new Quaternion());
+				}
+				Tilemap.SetTile(GridLayout.WorldToCell(new Vector3(transform.position.x + i, transform.position.y + j, transform.position.z)), null);
+			}
+		}
 	}
 }
